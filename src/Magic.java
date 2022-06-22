7aca0bb0c2a4cf7ef16200787adccc5ce5e5f10b
Aleksey <alexkox74@gmail.com>
2022-06-22 22:35:12 +0600

    Initial commit

---
+++ src/Magic.java
@@ -0,0 +1,6 @@
+public class Magic extends Hero{
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Magic бафнул тиму на доп урон");
+    }
+}
---
+++ src/Medic.java
@@ -0,0 +1,12 @@
+public class Medic extends Hero {
+    private int HealPoints;
+
+    public void increaseExperience(){
+        this.HealPoints += HealPoints * 0.1;
+        System.out.println("Medic увеличил кол-во единиц лечения");
+    }
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic вылечил команду");
+    }
+}
